956e9295954ef1efd7c24b2a8fdd87115038a27f
davidawoud <66363752+davidawoud@users.noreply.github.com>
2022-04-01 23:42:07 -0400

    final with errors

--- ww.c
+++ ww.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
 
 #define BUFFER_SIZE 128
 #define TRUE 1
@@ -14,30 +15,48 @@ int wrapfile(int input_file, int output_file, int width) {
 	int bufferOffset = 0, currLineLength = 0, currWordLength = 0;
 	int failure = FALSE;
 	int whitespaceSequence = FALSE, newLineCounter = 0;
+    char *buffer_leftover;
+    int buffer_leftover_size = 0;
 
 	while (read(input_file, buffer, BUFFER_SIZE) != 0) { //Keeps reading until no input is left
-		for (int x = 0; x < BUFFER_SIZE; x++) {
+        for (int x = 0; x < BUFFER_SIZE; x++) {
 			if (!isspace(buffer[x])) {  //Checks for non whitespace character
 				if (whitespaceSequence){ //Occurs when the first letter of the next word is found
 					whitespaceSequence = FALSE;
 					if (currWordLength + currLineLength < width) { //Checks to see if word fits
-						if (currLineLength != 0) { //If start of line, no space
-							write(output_file, ' ', 1);
+                        if (currLineLength != 0) { //If start of line, no space
+							write(output_file, " " , 1);
+                            currLineLength++;
 						}
-						write(output_file, buffer + bufferOffset, currWordLength);
+                        if (buffer_leftover_size != 0)
+                        {
+                            write(output_file, buffer_leftover, buffer_leftover_size);
+                            free(buffer_leftover);
+                            buffer_leftover = NULL;
+                            buffer_leftover_size = 0;
+                            write(output_file, buffer + bufferOffset, currWordLength - buffer_leftover_size);
+                        }
+                        else
+                        {
+                            write(output_file, buffer + bufferOffset, currWordLength);
+                        }
+                        currLineLength += currWordLength;
 						currWordLength = 0;
 						bufferOffset = x;
 					}
 					else
                     {
 						if (currWordLength > width) {
-                            write(output_file, '\n', 1);
+                            write(output_file, "\n", 1);
 							write(output_file, buffer + bufferOffset, currWordLength);
 							failure = TRUE;
+                            write(output_file, "\n", 1);
+                            currWordLength = 0;
+                            currLineLength = 0;
 						}
 						else
 						{
-							write(output_file, '\n', 1);
+							write(output_file, "\n", 1);
 							write(output_file, buffer+bufferOffset, currWordLength);
                             currLineLength = currWordLength;
 						}
@@ -45,7 +64,7 @@ int wrapfile(int input_file, int output_file, int width) {
                     }
 
                     if (newLineCounter >= 2) { //If there are >2 \n, new line
-						write(output_file,'\n\n', 2);
+						write(output_file,"\n\n", 2);
                         currLineLength = 0;
 					}
                     newLineCounter = 0;
@@ -62,10 +81,11 @@ int wrapfile(int input_file, int output_file, int width) {
 			}
 		}
 
-        char buffer_leftover[currWordLength];
-        for (int i = 0; i < currWordLength; i++)
+        buffer_leftover_size = currWordLength;
+        buffer_leftover = malloc(currWordLength * sizeof(char));
+        for (int i = 0; i < buffer_leftover_size; i++)
         {
-            buffer_leftover[i] = buffer[bufferOffset];
+            *(buffer_leftover + (i*sizeof(char))) = buffer[bufferOffset];
             bufferOffset++;
         }
         bufferOffset = 0;
@@ -82,6 +102,7 @@ int main(int argc, char **argv)
     int wrapping_width = 0;
     char *path;
     int input_fd = -1;
+    DIR * input_dir;
     struct stat input_stats;
     int output_fd = -1;
     if (argc < 2)
@@ -97,15 +118,15 @@ int main(int argc, char **argv)
     if (argc == 3)
     {
         wrapping_width = atoi(argv[1]);
-        *path = argv[2];
+        path = argv[2];
         // read from a file code
         stat(path, &input_stats);
         // Check for file existence
-        if (input_stats.st_mode & F_OK)
+        if (input_stats.st_mode)
         {
             if (S_ISDIR(input_stats.st_mode))
             {
-                input_fd = opendir(path);
+                input_dir = opendir(path);
             }
             else
             {
@@ -115,6 +136,7 @@ int main(int argc, char **argv)
         }
         else
         {
+            //puts(input_stats.st_mode);
             perror("Input file does not exist");
             return EXIT_FAILURE;
         }
@@ -126,10 +148,10 @@ int main(int argc, char **argv)
     }
 
     //check file or directory
-    if(S_ISDIR(input_fd))
+    if(S_ISDIR(input_stats.st_mode))
     {
         struct dirent *de;
-        de = readdir(input_fd);
+        de = readdir(input_dir);
         while (de != NULL)
         {
             char *file_name = de->d_name;
@@ -139,14 +161,15 @@ int main(int argc, char **argv)
                 output_fd = open(path + '/wrap.' + *file_name, O_WRONLY|O_CREAT|O_TRUNC, 600);
                 if(S_ISDIR(file_fd))
                 {
-                    de = readdir(input_fd);
+                    de = readdir(input_dir);
                     continue;
                 }
                 wrapfile(file_fd, output_fd, wrapping_width);
             }
 
-            de = readdir(input_fd);
+            de = readdir(input_dir);
         }
+        closedir(input_dir);
     }
     else
     {
